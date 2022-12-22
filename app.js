44701a0ddf0c44e677d79e9692fa6067340061bd
yujie-121 <94167770+yujie-121@users.noreply.github.com>
2022-12-23 00:00:22 +0100

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
-
+const data = require("./data.js");
 const app = express();
 
 app.engine(
@@ -15,5 +15,10 @@ app.use(express.static("public"));
 app.get("/", function (request, response) {
   response.render("start.hbs");
 });
-
+app.get("/movies", function (request, response) {
+  const model = {
+    movies: data.movies,
+  };
+  response.render("movies.hbs", model);
+});
 app.listen(8080);
---
+++ data.js
@@ -0,0 +1,17 @@
+exports.movies = [
+  {
+    id: 1,
+    title: "Thor: The Dark World",
+    grade: 9,
+  },
+  {
+    id: 2,
+    title: "Thor: Ragnarok",
+    grade: 8,
+  },
+  {
+    id: 3,
+    title: "Thor: Love and Thunder",
+    grade: 8,
+  },
+];
