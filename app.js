219f434b2e0c3718bb00477afdbb5efc6bf532c7
yujie-121 <94167770+yujie-121@users.noreply.github.com>
2022-12-26 21:35:00 +0100

    Stored resources in database instead

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const expressHandlebars = require("express-handlebars");
 const sqlite3 = require("sqlite3");
 
 const db = new sqlite3.Database("joeyworld-database.db");
-db.run(CREATE.TABLE.movie(id.INTEGER.PRIMARY.KEY, title.TEXT, grade.INTEGER));
+db.run(`
+	CREATE TABLE IF NOT EXISTS movies (
+		id INTEGER PRIMARY KEY,
+		title TEXT,
+		grade INTEGER
+	)
+`);
 const app = express();
 const bodyParser = require("body-parser");
 
@@ -24,11 +30,16 @@ app.use(
 app.get("/", function (request, response) {
   response.render("start.hbs");
 });
+
 app.get("/movies", function (request, response) {
-  const model = {
-    movies: data.movies,
-  };
-  response.render("movies.hbs", model);
+  const query = `SELECT * FROM movies`;
+
+  db.all(query, function (error, movies) {
+    const model = {
+      movies,
+    };
+    response.render("movies.hbs", model);
+  });
 });
 
 app.get("/create-movie", function (request, response) {
@@ -39,23 +50,25 @@ app.post("/create-movie", function (request, response) {
   const title = request.body.title;
   const grade = request.body.grade;
 
-  data.movies.push({
-    id: data.movies.length + 1,
-    title: title,
-    grade: grade,
-  });
+  const query = `INSERT INTO movies(title,grade) VALUES (?, ?)`;
+  const values = [title, grade];
 
-  response.redirect("/movies");
+  db.run(query, values, function (error) {
+    response.redirect("/movies");
+  });
 });
 
 app.get("/movies/:id", function (request, response) {
   const id = request.params.id;
-  const movie = data.movies.find((m) => m.id == id);
 
-  const model = {
-    movie: movie,
-  };
+  const query = `SELECT * FROM movies WHERE id=?`;
+  const values = [id];
 
-  response.render("movie.hbs", model);
+  db.get(query, values, function (error, movie) {
+    const model = {
+      movie,
+    };
+    response.render("movie.hbs", model);
+  });
 });
 app.listen(8080);
